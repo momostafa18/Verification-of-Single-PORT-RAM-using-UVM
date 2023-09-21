241cbd80055d011b4cadc0855f952b033f790410
Mohammed Mostafa <96149547+momostafa18@users.noreply.github.com>
2023-09-22 00:42:12 +0300

    Update TB.sv

--- TB.sv
+++ TB.sv
@@ -10,7 +10,9 @@ module TestBench();
 reg_file DUT(in1.RdEn,in1.WrEn,in1.Data_in,in1.Address,in1.CLK,in1.RST,in1.DataVLD,in1.Data_out);
   initial 
    begin
+     //setting the virtual interface inside the test class
      uvm_config_db#(virtual intf1)::set(null,"uvm_test_top","my_vif",in1);
+     //to run the UVM enviroment , run_test is used.
      run_test("Mo_test");
     end
   initial
